84344d1e5eb95189d6a1323b30907f5efe04fd48
Rafael Fernandes <>
2021-05-27 16:54:44 +0000

    + if statement limiting post only in workdays

--- main.py
+++ main.py
@@ -4,55 +4,46 @@ from pandas_cal import *
 from datetime import datetime
 
 
-
+#linkedin access
 url = 'https://api.linkedin.com/v2/ugcPosts'
-
-
 access_token = os.environ['access_token']
-
-
 urn = os.environ['urn']
-
-
 author = f"urn:li:person:{urn}"
-
-
-
 headers = {'X-Restli-Protocol-Version': '2.0.0',
            'Content-Type': 'application/json',
            'Authorization': f'Bearer {access_token}'}
 
+if datetime.today().weekday() <=4:
 
+  publication = welcome_msg + "\n" + df_styled.to_string(index=False)
 
-publication = welcome_msg + "\n" + df_styled.to_string(index=False)
+  def post_link():
+    api_url = url
 
-def post_link():
-  api_url = url
+    body = {
+        "author": author,
+        "lifecycleState": "PUBLISHED",
+        "specificContent": {
+              "com.linkedin.ugc.ShareContent": {
+                  "shareCommentary": {
+                      "text": publication
+                  },
+                  "shareMediaCategory": "NONE"
+              },
+          },
+          "visibility": {
+              "com.linkedin.ugc.MemberNetworkVisibility": "PUBLIC"
+          },
+      }
 
-  body = {
-      "author": author,
-      "lifecycleState": "PUBLISHED",
-      "specificContent": {
-            "com.linkedin.ugc.ShareContent": {
-                "shareCommentary": {
-                    "text": publication
-                },
-                "shareMediaCategory": "NONE"
-            },
-        },
-        "visibility": {
-            "com.linkedin.ugc.MemberNetworkVisibility": "PUBLIC"
-        },
-    }
+    response = requests.post(api_url,headers=headers,json=body)
 
-  response = requests.post(api_url,headers=headers,json=body)
+    if response.status_code == 201:
+          print("Success")
+          print(response.content)
+    else:
+          print(response.content)
 
-  if response.status_code == 201:
-        print("Success")
-        print(response.content)
-  else:
-        print(response.content)
-
-post_link()
+  post_link()
 
 
